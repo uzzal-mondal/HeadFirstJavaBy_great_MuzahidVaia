bf74175461f78cf43624482a6030c4d828844a0e
uzzal <36932200+uzzalcontact@users.noreply.github.com>
2019-10-03 09:29:47 +0600

    Asignment Method Call In Java HeadFirst

---
+++ AssignmentMethodCall_1.java
@@ -0,0 +1,32 @@
+package HeadFirstjava.BasicHeadJava;
+
+public class AssignmentMethodCall_1 {
+
+    public static void main(String args[]) {
+
+        // firstly variable initialize
+        int a = 10;
+        String name = "Rifat Vaia";
+
+        // a=10 then 10 is storage in value a !
+
+        a = a * 7;
+        System.out.println("The is num a =  " + a);
+
+
+        //declare to now random value
+        double i = 10;
+        i = Math.random();
+        System.out.println("The Random num: " + i);
+
+
+        /*this is output show the random num with asign operator.
+          The is num a =  70
+         The Random num: 0.7445058451301356
+        */
+
+
+    }
+
+
+}
