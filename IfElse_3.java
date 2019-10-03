966e30de277585e7a64a7334c5b46cb568371f84
uzzal <36932200+uzzalcontact@users.noreply.github.com>
2019-10-03 09:32:57 +0600

    If Else Condition By Head First Java

---
+++ IfElse_3.java
@@ -0,0 +1,28 @@
+package HeadFirstjava.BasicHeadJava;
+
+public class IfElse_3 {
+
+    public static void main(String args[]) {
+
+        int x = 10;
+
+        if (x == 10) {
+
+            System.out.println("X must be:  " + x);
+        } else {
+
+            System.out.println("X isn't  10");
+
+        }
+
+
+        // important
+        int a = 1;
+        String name = "Mujahid vaia";
+
+        if (((a < 5) & (name.equals("Mujahid vaia")))){
+
+            System.out.println("Gentely");
+        }
+    }
+}
